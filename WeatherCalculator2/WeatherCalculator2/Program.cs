0ed1363f44a3c6e3e44e1c4ac893e0594afd051f
rschmidt15 <59981860+rschmidt15@users.noreply.github.com>
2020-01-30 16:37:39 -0600

    heat index
    1/30 work done

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -6,12 +6,14 @@ namespace WeatherCalculator2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("### Weather Calculator 2");
+            Console.WriteLine("### Weather Calculator 2 Roman Schmidt");
 
             int temperature;
             int windSpeed;
             double windChill;
             string input = "";
+            int RH;
+            double Heatindex;
 
             // Get the temperature from the user
             Console.WriteLine("Enter the temperature");
@@ -19,19 +21,43 @@ namespace WeatherCalculator2
             input = Console.ReadLine();
 
             temperature = int.Parse(input);
+            if (temperature < 50)
+            {
+                Console.WriteLine("you are cold");
+                // Get the windspeed from the user
+                Console.WriteLine("Enter the wind speed");
 
-            // Get the windspeed from the user
-            Console.WriteLine("Enter the wind speed");
+                input = Console.ReadLine();
 
-            input = Console.ReadLine();
+                windSpeed = int.Parse(input);
 
-            windSpeed = int.Parse(input);
+                // Calculate the wind chill
 
-            // Calculate the wind chill
+                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
 
-            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+                Console.WriteLine("The wind chill is " + windChill);
+            }
+            else if (temperature>=50)
+            { Console.WriteLine("you are happy");
+                Console.WriteLine("Enter the Relative humidity");
 
-            Console.WriteLine("The wind chill is " + windChill);
+                input = Console.ReadLine();
+
+                RH = int.Parse(input);
+
+                // Calculate the wind chill
+
+                Heatindex = 0.5*(temperature+61+((temperature-68)*1.2+(RH*0.094)));
+
+                Console.WriteLine("The heatindex is " + Heatindex);
+            }
+
+
+
+           
+            else { Console.WriteLine("your data is invalid"); }
+
+
         }
     }
 }
